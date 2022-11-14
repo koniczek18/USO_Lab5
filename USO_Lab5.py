c46a72792bedde6b1aaeaae66a79b81be37c5ff7
Szymon <sz.zalas@gmail.com>
2022-11-14 19:51:17 +0100

    Initial commit

---
+++ USO_Lab5.py
@@ -0,0 +1,10 @@
+from scipy.integrate import odeint
+import numpy as np
+import scipy
+import matplotlib.pyplot as plt
+
+def print_hi(name):
+    print(f'Hi, {name}')
+
+if __name__ == '__main__':
+    print_hi('PyCharm')
